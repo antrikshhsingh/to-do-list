f89446bb14af5de5fcc0e4a2f060aeb675c0939b
antrikshhsingh <antrikshsingh@Antrikshs-MacBook-Air.local>
2024-01-19 22:34:57 +0530

    dustbin

--- src/TodoItem.jsx
+++ src/TodoItem.jsx
@@ -1,23 +1,19 @@
-/* eslint-disable no-unused-vars */
-/* eslint-disable react/prop-types */
-// eslint-disable-next-line no-unused-vars
+import Icon from "./Icon";
+
 export function TodoItem({ completed, id, title, toggleTodo, deleteTodo }) {
-    return(
-        <li>
-            <label>
-                <input 
-                  type="checkbox" 
-                  checked={completed} 
-                  onChange={e => toggleTodo(id, e.target.checked)}
-                />
-                {title}
-            </label>
-            <button 
-                onClick={() => deleteTodo(id)}
-                className="btn btn-danger"
-            >
-            <img src = "pngegg.png" alt="Dustbin" width="30" height="30" />
-            </button>
-        </li>
-    )
+  return (
+    <li>
+      <label>
+        <input
+          type="checkbox"
+          checked={completed}
+          onChange={(e) => toggleTodo(id, e.target.checked)}
+        />
+        {title}
+      </label>
+      <button onClick={() => deleteTodo(id)} className="btn btn-danger">
+        <Icon />
+      </button>
+    </li>
+  );
 }
